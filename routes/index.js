31e7c779b40765e8abfaa8320fdad082b870644d
drmehta1996 <dm27091996@gmail.com>
2020-07-10 13:25:31 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,13 @@ var router = express.Router();
 var path = require("path");
 const multer = require("multer");
 var firebase = require("firebase-admin");
-var moment = require("moment");
+var moment = require("moment-timezone");
 var companySchema = require("../models/company.models");
 var subcompanySchema = require("../models/subcompany.models");
 var employeeSchema = require("../models/employee.model");
 var attendeanceSchema = require("../models/attendance.models");
 var timingSchema = require("../models/timing.models");
+const { stat } = require("fs");
 var attendImg = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads");
@@ -482,43 +483,49 @@ router.post("/attendance", upload.single("attendance"), async function (
   next
 ) {
   moment.locale("en-in");
-  var date = moment().format("DD MM YYYY, h:mm:ss a").split(",")[0];
-  var time = moment().format("DD MM YYYY, h:mm:ss a").split(",")[1];
-  var day = moment().format("dddd");
+  var date = moment()
+    .tz("Asia/Calcutta")
+    .format("DD MM YYYY, h:mm:ss a")
+    .split(",")[0];
+  date = date.split(" ");
+  date = date[0] + "/" + date[1] + "/" + date[2];
+  var time = moment()
+    .tz("Asia/Calcutta")
+    .format("DD MM YYYY, h:mm:ss a")
+    .split(",")[1];
+  var day = moment().tz("Asia/Calcutta").format("dddd");
   if (req.body.type == "in") {
     var longlat = await employeeSchema
       .find({ _id: req.body.employeeid })
       .populate("SubCompany");
-    lon1 = req.body.longitude;
-    lon2 = longlat[0]["SubCompany"].long;
-    lat1 = req.body.latitude;
-    lat2 = longlat[0]["SubCompany"].lat;
-    console.log(
-      "User-" + lon1 + " Comapny-" + lon2 + " User-" + lat1 + " Company-" + lat2
-    );
-    unit = "K";
-    var radlat1 = (Math.PI * lat1) / 180;
-    var radlat2 = (Math.PI * lat2) / 180;
-    var theta = lon1 - lon2;
-    var radtheta = (Math.PI * theta) / 180;
-    var dist =
-      Math.sin(radlat1) * Math.sin(radlat2) +
-      Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-    if (dist > 1) {
-      dist = 1;
+    if (longlat[0]["SubCompany"].long != "") {
+      lon1 = req.body.longitude;
+      lon2 = longlat[0]["SubCompany"].long;
+      lat1 = req.body.latitude;
+      lat2 = longlat[0]["SubCompany"].lat;
+      unit = "K";
+      var radlat1 = (Math.PI * lat1) / 180;
+      var radlat2 = (Math.PI * lat2) / 180;
+      var theta = lon1 - lon2;
+      var radtheta = (Math.PI * theta) / 180;
+      var dist =
+        Math.sin(radlat1) * Math.sin(radlat2) +
+        Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = (dist * 180) / Math.PI;
+      dist = dist * 60 * 1.1515;
+      if (unit == "K") {
+        dist = dist * 1.609344;
+      }
+      if (unit == "N") {
+        dist = dist * 0.8684;
+      }
+      var fd = dist * 1000;
+      var area = fd > 100 ? "Outside Area" : longlat[0]["Subcompany"].Name;
     }
-    dist = Math.acos(dist);
-    dist = (dist * 180) / Math.PI;
-    dist = dist * 60 * 1.1515;
-    if (unit == "K") {
-      dist = dist * 1.609344;
-    }
-    if (unit == "N") {
-      dist = dist * 0.8684;
-    }
-    var fd = dist * 1000;
-    var area = fd > 100 ? "Outside Area" : longlat[0]["Subcompany"].Name;
-    console.log(area);
     if (req.body.filename == undefined) {
       var record = attendeanceSchema({
         EmployeeId: req.body.employeeid,
@@ -586,17 +593,43 @@ router.post("/attendance", upload.single("attendance"), async function (
       res.json(result);
     });
   } else if (req.body.type == "getdata") {
-    if (req.body.afilter == 0) {
-      var record = await attendeanceSchema.find({}).populate("EmployeeId");
-    } else if (req.body.afilter == 1) {
-      var record = await attendeanceSchema
-        .find({ Area: "Inside Area" })
-        .populate("EmployeeId");
-    } else {
-      var record = await attendeanceSchema
-        .find({ Area: "Outside Area" })
-        .populate("EmployeeId");
+    const day = req.body.day;
+    const sdate = req.body.sd == "" ? undefined : req.body.sd;
+    const edate = req.body.ed == "" ? undefined : req.body.ed;
+    const area = req.body.afilter;
+    const status = req.body.status;
+    let query = {};
+    if (req.body.rm == 0) {
+      if (day) {
+        if (day != "All") {
+          query.Day = day;
+        }
+      }
+      if (sdate != undefined || edate != undefined) {
+        query.Date = {
+          $gte: sdate,
+          $lte: edate,
+        };
+      }
+      if (area) {
+        if (area == 0) {
+        } else if (area == 1) {
+          query.Area = "Inside Area";
+        } else {
+          query.Area = "Outside Area";
+        }
+      }
+      if (status) {
+        if (status == 0) {
+        } else if (status == 1) {
+          query.Status = "in";
+        } else if (status == 2) {
+          query.Status = "out";
+        }
+      }
     }
+    console.log(query);
+    var record = await attendeanceSchema.find(query).populate("EmployeeId");
     var result = {};
     if (record.length == 0) {
       result.Message = "Attendance Not Found";
@@ -737,8 +770,11 @@ router.post("/timing", (req, res) => {
   }
 });
 
-// router.post("/testing", async (req, res) => {
-//   console.log(date);
-//   console.log(time);
-//   });
+router.post("/testing", async (req, res) => {
+  result = await attendeanceSchema.find({
+    Day: "Thursday",
+    Date: { $gte: "09/07/2020", $lte: "09/07/2020" },
+  });
+  res.json(result);
+});
 module.exports = router;
