a55d814f86a9d35362fc781e92d1b9cc7910c2a4
drmehta1996 <dm27091996@gmail.com>
2020-07-13 17:59:29 +0530

    Code Changes

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use("uploads", express.static(__dirname + "uploads"));
 app.use(cors());
 
 app.use("/api/", indexRouter);
