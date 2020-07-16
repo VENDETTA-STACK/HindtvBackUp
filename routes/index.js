6035cf9587c7e3a54a9d3b58d2dd41409aef31b7
drmehta1996 <dm27091996@gmail.com>
2020-07-16 15:04:36 +0530

    Code Changes

--- routes/index.js
+++ routes/index.js
@@ -944,6 +944,33 @@ router.post("/thought", (req, res) => {
       }
       res.json(result);
     });
+  } else if (req.body.type == "update") {
+    thoughtSchema.findByIdAndUpdate(
+      req.body.id,
+      {
+        Quote: req.body.quote,
+        Name: req.body.name,
+      },
+      (err, record) => {
+        var result = {};
+        if (err) {
+          result.Message = "Thought Not Updated";
+          result.Data = [];
+          result.isSuccess = false;
+        } else {
+          if (record.length == 0) {
+            result.Message = "Thought Not Updated";
+            result.Data = [];
+            result.isSuccess = false;
+          } else {
+            result.Message = "Thought Updated";
+            result.Data = record;
+            result.isSuccess = true;
+          }
+        }
+        res.json(result);
+      }
+    );
   }
 });
 
