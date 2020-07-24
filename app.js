af4d3e8895898e75730a596aad441bca96971c8d
drmehta1996 <dm27091996@gmail.com>
2020-07-24 15:51:49 +0530

    hind tv

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("uploads", express.static(__dirname + "uploads"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use("/reports", express.static(__dirname + "/reports"));
 app.use(cors());
 
 app.use("/", indexRouter);
