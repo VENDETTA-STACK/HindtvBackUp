338af8512a451052b66b506e31c8de353742a876
ItFuturzTech9Teen <jagannathtech9teen@gmail.com>
2020-08-28 13:27:23 +0530

    hghsd

--- models/attendance.models.js
+++ models/attendance.models.js
@@ -1,22 +1,22 @@
 var mongoose = require("mongoose");
 
 var newSchema = mongoose.Schema({
-  EmployeeId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "employee",
-    required: true,
-  },
-  Day: String,
-  Time: String,
-  Date: String,
-  Image: String,
-  Status: String,
-  Area: String,
-  Elat: String,
-  Elong: String,
-  Distance: String,
-  Memo: Number,
-  wifiName: String,
+    EmployeeId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "employee",
+        required: true,
+    },
+    Day: String,
+    Time: String,
+    Date: String,
+    Image: String,
+    Status: String,
+    Area: String,
+    Elat: String,
+    Elong: String,
+    Distance: String,
+    Memo: Number,
+    wifiName: String,
 });
 
 const admin = mongoose.model("attendance", newSchema);
